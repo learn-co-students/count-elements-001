bf89dc2b37a23bf25d6578a47a668a4dfa39c1a2
halfields <hfields697@aol.com>
2015-10-25 03:22:09 +0000

    done

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,10 @@
 def count_elements(array)
-  # code goes here
+  hash = {}
+  array.each do |element|
+    if hash.key?(element)
+      hash[element] += 1
+    else hash[element] = 1
+    end
+  end
+  hash
 end
- 
