19a27e2dbdb644db5b2045d8204385f876e577ff
christianbm1 <christianbm1@gmail.com>
2015-10-30 10:16:44 -0400

    done

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,17 @@
 def count_elements(array)
   # code goes here
+  ash = {}
+  count = 0
+  arr = array.uniq
+    arr.each do |val|
+      array.each do |value|
+        if val == value then
+          count +=1
+        end
+      end
+      ash[val] = count
+      count = 0
+    end
+    return ash
 end
  
