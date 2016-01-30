cf2502f039b7b447e8daec09ccacce867e433945
epang1 <epang1@gmail.com>
2016-01-30 17:40:22 -0500

    done

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,12 @@
 def count_elements(array)
-  # code goes here
+  new_hash = {}
+  array.each do |animal|
+    if new_hash[animal] == nil
+      new_hash[animal] = 1
+    else 
+      new_hash[animal] += 1
+    end
+  end
+  return new_hash
 end
  
