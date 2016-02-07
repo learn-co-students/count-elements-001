5076dd471372467170a24efc2bfba8dcaf56215f
gregmarquet <marquet.greg@gmail.com>
2016-02-07 14:40:26 -0500

    count_elements takes an array and return an hash where the elements of the array become the keys and the number of repetition of the elements in the array become the value

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,10 @@
 def count_elements(array)
-  # code goes here
+  array.each_with_object({}) do |item, hash|
+    if hash[item]
+      hash[item] += 1 
+    else 
+      hash[item] = 1
+    end
+  end
 end
  
