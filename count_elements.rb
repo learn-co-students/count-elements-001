c1aea473a5e0f96ad630db367e124298dbf8041e
fhernandez173 <f.hernandez173@gmail.com>
2016-03-07 13:09:18 -0500

    done

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,17 @@
+require 'pry'
+
 def count_elements(array)
-  # code goes here
+  # set ampty array
+  new_hash = Hash.new
+  # interate through array and use variable to keep track of # of animals mentioned
+  array.each{ |animal|
+    # ||= means that if the key in this case does not exist, it creates it
+    new_hash[animal] ||= 0
+    new_hash[animal] += 1
+  }
+  new_hash
+  # set strings as keys and point it to its value
 end
- 
+
+
+
